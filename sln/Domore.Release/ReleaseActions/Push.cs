dc18300826e5dd1ca8f9b5f18caf507bf11d313b
Ken Yourek <ken@yourek.net>
2019-04-26 15:46:13 -0500

    Work on build tools.

--- sln/Domore.Release/ReleaseActions/Push.cs
+++ sln/Domore.Release/ReleaseActions/Push.cs
@@ -1,12 +1,15 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace Domore.ReleaseActions {
     class Push : ReleaseAction {
         public string Source { get; set; }
+        public IDictionary<string, string> Project { get; set; } = new Dictionary<string, string>();
 
         public override void Work() {
-            foreach (var pkg in new[] { "Domore.Configuration", "Domore.Configuration.ConfigurationManager" }) {
-                var pkgFile = Path.Combine(SolutionDirectory, $"{pkg}.{Context.Version.StagedVersion}.nupkg");
+            foreach (var project in Project.Values) {
+                var pkgDir = Path.Combine(SolutionDirectory, project, "bin", "Release");
+                var pkgFile = Path.Combine(pkgDir, $"{project}.{Context.Version.StagedVersion}.nupkg");
                 var pkgSource = Source?.Trim() ?? "";
                 Process("nuget", "push", $"\"{pkgFile}\"", "-Source", pkgSource == "" ? "nuget.org" : pkgSource);
             }
